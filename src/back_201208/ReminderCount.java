19accdee32170ada328b68286477d29b9c5a968c
simpson <450054@naver.com>
2020-12-09 18:24:10 +0900

    4.CountReminder

--- src/back_201208/ReminderCount.java
+++ src/back_201208/ReminderCount.java
@@ -1,6 +1,8 @@
 package back_201208;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class ReminderCount {
 	public static void remind() {
@@ -14,22 +16,33 @@ public class ReminderCount {
 		
 		int[] remind = new int[10];
 		for(int i = 0; i<10; i++) {
-			remind[i] = numArray[i] % 2;
+			remind[i] = numArray[i] % 3;
 		}
 		Array.array(remind);
 		
 		int count = 1;
-		int[] k = null;
-		
+		ArrayList<Integer> simp = new ArrayList<Integer>();
 		for(int i = 0; i<10; i++) {
-			for(int j = i + 1; j<10; j++) {
-				if(remind[i] != remind[j]) {
-					count++;
-				}
-				else {
-				}
-			}
+			simp.add(remind[i]);
 		}
-		System.out.println("서로 다른 나머지는 " + count + "개 입니다");
+		
+		//TreeSet으로 정렬
+		TreeSet<Integer> simp2 = new TreeSet<Integer>(simp);
+		
+		//TreeSet 값을 다시 배열에 대입
+		ArrayList<Integer> simp3 = new ArrayList<Integer>(simp2);
+		
+		for(int print : simp) {
+			System.out.print("["+print+"] ");
+		}
+		
+		System.out.println("");
+		
+		for(int print : simp3) {
+			System.out.print("["+print+"] ");
+		}
+		
+		System.out.println("");
+		System.out.println("서로 다른 나머지는 " + simp3.size() + "개 입니다");
 	}
 }
